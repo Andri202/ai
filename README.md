fe3690af5b4a5cd4002483bb19cdef7de16c8ed3
Andri202 <andrign20@gmail.com>
2026-04-27 00:23:07 +0700

    add bug analyzer capability

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# ai
-please create a .env file based on .env.example
+# Setup the agent
 
-pip install openai python-dotenv
-pip install langchain-community
+create the folder project:
+	touch ai-qa-agent
+    mkdir ai-qa-agent
+	cd ai-qa-agent
+
+create virtual env:
+	python -m venv venv
+	source venv/bin/activate
+
+install the requirements and set up the .env
+    pip install -r requirements.txt
+    create a .env file based on .env.example
+
+
+happy testing!!!
+
--- main.py
+++ main.py
@@ -7,21 +7,44 @@ load_dotenv()
 # client = OpenAI()
 llm = Ollama(model='llama3')
 
-
 def generate_test_cases(req):
     prompt =f"""act as a qa engineer.
     create test cases for:
     {req}
 
-    include positive, negative edge cases"""
+    includes:
+    1.positive
+    2.negative 
+    3.edge cases"""
 
     # response = client.chat.completions.create(
     #     model="gpt-4o-mini",
     #     messages=[{"role":"user", "content":prompt}]
     # )
 
-    response = llm.invoke(prompt)
+    return llm.invoke(prompt)
 
-    return response
 
-print(generate_test_cases("User Login with OTP"))
+def bug_analyzer(bug):
+    prompt = f"""
+    act as a qa engineer
+    analyzing this bug:{bug}
+
+    give:
+    1. bug type
+    2. severity
+    3. possible root cause
+    4. reprodcution steps 
+    """
+
+    return llm.invoke(prompt)
+
+def qa_agent(task, text):
+    if task.lower() == "test":
+        return generate_test_cases(text)
+    elif task.lower() == "bug":
+        return bug_analyzer(text)
+    else:
+        return "unknown task!!"
+
+print(qa_agent("test","User Login with OTP"))
